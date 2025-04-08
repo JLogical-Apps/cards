b37e43cf65550b6d26d08a2468b7c7f4b0cf5945
jaboyc <jake@jlogical.com>
2025-04-08 15:56:00 -0400

    Improve horizontal layout.

--- lib/widgets/card_scaffold.dart
+++ lib/widgets/card_scaffold.dart
@@ -7,6 +7,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:provider/provider.dart';
 import 'package:solitaire/context/card_game_context.dart';
 import 'package:solitaire/home_page.dart';
+import 'package:solitaire/utils/constraints_extensions.dart';
 import 'package:solitaire/utils/duration_extensions.dart';
 
 class CardScaffold extends HookWidget {
@@ -52,104 +53,179 @@ class CardScaffold extends HookWidget {
       confettiController.stop();
     }
 
-    return Stack(
-      children: [
-        Column(
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final axis = constraints.largestAxis;
+
+        return Stack(
           children: [
-            Expanded(
-              child: SafeArea(
-                bottom: false,
-                child: Padding(
-                  padding: EdgeInsets.all(4),
-                  child: LayoutBuilder(builder: builder),
-                ),
-              ),
-            ),
-            if (!isPreview)
-              Container(
-                height: max(MediaQuery.paddingOf(context).bottom + 32, 48),
-                color: Colors.white.withValues(alpha: 0.2),
-                alignment: Alignment.center,
-                child: SafeArea(
-                  top: false,
-                  bottom: false,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 24),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Tooltip(
-                          message: 'Menu',
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(backgroundColor: Colors.white.withValues(alpha: 0.5)),
-                            onPressed: () async {
-                              await showAdaptiveActionSheet(
-                                context: context,
-                                actions: [
-                                  BottomSheetAction(
-                                    title: Text('New Game'),
-                                    leading: Icon(Icons.star_border),
-                                    onPressed: (context) {
-                                      startTimeState.value = DateTime.now();
-                                      onNewGame();
-                                      Navigator.of(context).pop();
-                                    },
-                                  ),
-                                  BottomSheetAction(
-                                    title: Text('Restart Game'),
-                                    leading: Icon(Icons.restart_alt),
-                                    onPressed: (_) {
-                                      startTimeState.value = DateTime.now();
-                                      onRestart();
-                                      Navigator.of(context).pop();
-                                    },
-                                  ),
-                                  BottomSheetAction(
-                                    title: Text('Close'),
-                                    leading: Icon(Icons.close),
-                                    onPressed: (_) => Navigator.of(context)
-                                        .pushReplacement(MaterialPageRoute(builder: (_) => HomePage())),
-                                  ),
-                                ],
-                              );
-                            },
-                            child: Icon(Icons.menu),
-                          ),
+            Column(
+              children: [
+                if (!isPreview && axis == Axis.horizontal)
+                  Container(
+                    height: max(MediaQuery.paddingOf(context).top + 32, 48),
+                    alignment: Alignment.center,
+                    child: SafeArea(
+                      top: false,
+                      bottom: false,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          spacing: 8,
+                          children: [
+                            Tooltip(
+                              message: 'Menu',
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(backgroundColor: Colors.white.withValues(alpha: 0.5)),
+                                onPressed: () async {
+                                  await showAdaptiveActionSheet(
+                                    context: context,
+                                    actions: [
+                                      BottomSheetAction(
+                                        title: Text('New Game'),
+                                        leading: Icon(Icons.star_border),
+                                        onPressed: (context) {
+                                          startTimeState.value = DateTime.now();
+                                          onNewGame();
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                      BottomSheetAction(
+                                        title: Text('Restart Game'),
+                                        leading: Icon(Icons.restart_alt),
+                                        onPressed: (_) {
+                                          startTimeState.value = DateTime.now();
+                                          onRestart();
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                      BottomSheetAction(
+                                        title: Text('Close'),
+                                        leading: Icon(Icons.close),
+                                        onPressed: (_) => Navigator.of(context)
+                                            .pushReplacement(MaterialPageRoute(builder: (_) => HomePage())),
+                                      ),
+                                    ],
+                                  );
+                                },
+                                child: Icon(Icons.menu),
+                              ),
+                            ),
+                            Tooltip(
+                              message: 'Undo',
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(backgroundColor: Colors.white.withValues(alpha: 0.5)),
+                                onPressed: onUndo,
+                                child: Icon(Icons.undo),
+                              ),
+                            ),
+                            Text(
+                              currentTimeState.value.difference(startTimeState.value).format(),
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
                         ),
-                        Text(
-                          currentTimeState.value.difference(startTimeState.value).format(),
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        Tooltip(
-                          message: 'Undo',
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(backgroundColor: Colors.white.withValues(alpha: 0.5)),
-                            onPressed: onUndo,
-                            child: Icon(Icons.undo),
-                          ),
-                        ),
-                      ],
+                      ),
+                    ),
+                  ),
+                Expanded(
+                  child: SafeArea(
+                    bottom: axis == Axis.vertical,
+                    child: Padding(
+                      padding: EdgeInsets.all(4),
+                      child: LayoutBuilder(builder: builder),
                     ),
                   ),
                 ),
+                if (!isPreview && axis == Axis.vertical)
+                  Container(
+                    height: max(MediaQuery.paddingOf(context).bottom + 32, 48),
+                    color: Colors.white.withValues(alpha: 0.2),
+                    alignment: Alignment.center,
+                    child: SafeArea(
+                      top: false,
+                      bottom: false,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 24),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Tooltip(
+                              message: 'Menu',
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(backgroundColor: Colors.white.withValues(alpha: 0.5)),
+                                onPressed: () async {
+                                  await showAdaptiveActionSheet(
+                                    context: context,
+                                    actions: [
+                                      BottomSheetAction(
+                                        title: Text('New Game'),
+                                        leading: Icon(Icons.star_border),
+                                        onPressed: (context) {
+                                          startTimeState.value = DateTime.now();
+                                          onNewGame();
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                      BottomSheetAction(
+                                        title: Text('Restart Game'),
+                                        leading: Icon(Icons.restart_alt),
+                                        onPressed: (_) {
+                                          startTimeState.value = DateTime.now();
+                                          onRestart();
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                      BottomSheetAction(
+                                        title: Text('Close'),
+                                        leading: Icon(Icons.close),
+                                        onPressed: (_) => Navigator.of(context)
+                                            .pushReplacement(MaterialPageRoute(builder: (_) => HomePage())),
+                                      ),
+                                    ],
+                                  );
+                                },
+                                child: Icon(Icons.menu),
+                              ),
+                            ),
+                            Text(
+                              currentTimeState.value.difference(startTimeState.value).format(),
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            Tooltip(
+                              message: 'Undo',
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(backgroundColor: Colors.white.withValues(alpha: 0.5)),
+                                onPressed: onUndo,
+                                child: Icon(Icons.undo),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: ConfettiWidget(
+                confettiController: confettiController,
+                blastDirectionality: BlastDirectionality.explosive,
+                emissionFrequency: 0.02,
+                numberOfParticles: 50,
+                maxBlastForce: 100,
+                minBlastForce: 60,
+                gravity: 0.3,
+                shouldLoop: true,
               ),
+            ),
           ],
-        ),
-        Align(
-          alignment: Alignment.topCenter,
-          child: ConfettiWidget(
-            confettiController: confettiController,
-            blastDirectionality: BlastDirectionality.explosive,
-            emissionFrequency: 0.02,
-            numberOfParticles: 50,
-            maxBlastForce: 100,
-            minBlastForce: 60,
-            gravity: 0.3,
-            shouldLoop: true,
-          ),
-        ),
-      ],
+        );
+      },
     );
   }
 }
